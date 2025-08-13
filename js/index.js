9b1f6b5461f49f90b19f96665de175c7295cd426
yoshihiroXYZ <157252044+yoshihiroXYZ@users.noreply.github.com>
2025-08-13 16:13:11 +0900

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// ハンバーガーボタンとドロワー
-$("#js-button-drawer").on("click", function () {
-    $(this).toggleClass("is-checked");
-    $("#js-drawer").slideToggle();
-    $("body").toggleClass("is-fixed");
-});
